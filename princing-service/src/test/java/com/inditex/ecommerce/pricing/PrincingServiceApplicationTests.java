0b11e147aaf375a2448f290c072d12b519443f09
jesusalbertobaute <jesusalbertobaute@gmail.com>
2026-09-26 14:02:30 +0200

    feat: add princing service

---
+++ princing-service/src/test/java/com/inditex/ecommerce/pricing/PrincingServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inditex.ecommerce.pricing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrincingServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
